afbe7ab82aef1c1cd8b85334d7f8043b1ba3a76b
David Castro <castro.pedav23@cordoba.salesianos.edu>
2025-05-09 00:19:01 +0200

    Creación de nuevas ventanas e importación del menu de navigación inferior

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.stagemaster"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.stagemaster"
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/stagemaster/MainActivity.kt
+++ app/src/main/java/com/example/stagemaster/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.stagemaster
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,36 +14,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stagemaster.ui.theme.StageMasterTheme
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var menuNavegacion: BottomNavigationView
+    private lateinit var configuracionFragment: ConfiguracionFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            StageMasterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContentView(R.layout.activity_main)
+
+        menuNavegacion = findViewById(R.id.menuNavegacion)
+        configuracionFragment = ConfiguracionFragment()
+
+
+        menuNavegacion.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.seccionConfiguracion -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container,configuracionFragment).commit()
+                    true
                 }
+
+                else -> false
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StageMasterTheme {
-        Greeting("Android")
     }
 }
