1fa8032a394aa5028281ceb1b683ead32a162b57
David Castro <castro.pedav23@cordoba.salesianos.edu>
2025-05-22 00:19:58 +0200

    Configuración de fragmentos del menu principal y buscar eventos

--- app/src/main/java/com/example/stagemaster/vistas/BuscarFragment.kt
+++ app/src/main/java/com/example/stagemaster/vistas/BuscarFragment.kt
@@ -1,24 +1,60 @@
 package com.example.stagemaster.vistas
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.stagemaster.R
+import com.example.stagemaster.controlador.EventoController
+import com.example.stagemaster.modeloBBDD.Evento
 
 class BuscarFragment: Fragment() {
+    private var mAdapter: ControladorRecylerView? = null
+    private lateinit var recyclerViewBuscar: RecyclerView
+    private lateinit var inputBuscar: EditText
+    private lateinit var imageViewBuscar: ImageView
+
+    private var eventosListBuscar = ArrayList<Evento>()
+    private lateinit var controladorEvento: EventoController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_buscar,container,false)
+
+        inputBuscar = rootView.findViewById(R.id.inputBuscar)
+        imageViewBuscar = rootView.findViewById(R.id.imageViewBuscar)
+        controladorEvento = EventoController(requireContext())
+
+        recyclerViewBuscar = rootView.findViewById(R.id.recyclerViewBuscar)
+        recyclerViewBuscar.layoutManager = LinearLayoutManager(requireContext())
+        mAdapter = ControladorRecylerView(eventosListBuscar)
+        recyclerViewBuscar.adapter = mAdapter
+
+        imageViewBuscar.setOnClickListener {
+            seleccionarEventosBuscar()
+        }
+
         return rootView
+    }
+
+    private fun seleccionarEventosBuscar(){
+        val eventosResultantes = controladorEvento.selectEventosNombres(inputBuscar.text.toString())
+        eventosListBuscar.clear()
+        eventosListBuscar.addAll(eventosResultantes)
+        mAdapter?.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/stagemaster/vistas/MenuPrincipalFragment.kt
+++ app/src/main/java/com/example/stagemaster/vistas/MenuPrincipalFragment.kt
@@ -5,9 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.stagemaster.R
+import com.example.stagemaster.controlador.ArtistaController
+import com.example.stagemaster.controlador.EventoController
+import com.example.stagemaster.modeloBBDD.Evento
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MenuPrincipalFragment: Fragment() {
+    private var mAdapter: ControladorRecylerView? = null
+    private lateinit var recyclerViewDestacados: RecyclerView
+    private lateinit var recyclerViewOtros: RecyclerView
+    private var eventosListDestacados = ArrayList<Evento>()
+    private var eventosListOtros = ArrayList<Evento>()
+
+    private lateinit var controladorEvento: EventoController
+    private lateinit var controladorArtista: ArtistaController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +36,51 @@ class MenuPrincipalFragment: Fragment() {
         val rootView : View = inflater.inflate(R.layout.fragment_menu_principal, container, false)
         rootView.tag = "MenuPrincipal Fragment"
 
+        controladorArtista = ArtistaController(requireContext())
+        controladorEvento = EventoController(requireContext())
+
+        if (controladorEvento.selectEventosEntradas().isEmpty()) {
+            insertarArtistas()
+            insertarEventos()
+        }
+
+
+        insertarOtrosEventos()
+
+        recyclerViewDestacados = rootView.findViewById(R.id.recyclerViewDestacados)
+        recyclerViewDestacados.layoutManager = LinearLayoutManager(requireContext())
+        insertarEventosDestacados()
+        mAdapter = ControladorRecylerView(eventosListDestacados)
+        recyclerViewDestacados.adapter = mAdapter
+
+
+        recyclerViewOtros = rootView.findViewById(R.id.recyclerViewOtros)
+        recyclerViewOtros.layoutManager = LinearLayoutManager(requireContext())
+        insertarOtrosEventos()
+        mAdapter = ControladorRecylerView(eventosListOtros)
+        recyclerViewOtros.adapter = mAdapter
+
         return rootView
+    }
+
+    private fun insertarEventosDestacados() {
+        eventosListDestacados = controladorEvento.selectEventosEntradas()
+    }
+
+    private fun insertarOtrosEventos() {
+        eventosListOtros = controladorEvento.selectOtrosEventos()
+    }
+
+    private fun insertarArtistas() {
+        val formatoFecha = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        controladorArtista.insertarArtista("David", "España", formatoFecha.parse("2020-03-21") , R.drawable.home)
+        controladorArtista.insertarArtista("Rafael", "España", formatoFecha.parse("2020-03-21") , R.drawable.home)
+        controladorArtista.insertarArtista("Carlos", "España", formatoFecha.parse("2020-03-21") , R.drawable.home)
+    }
+
+    private fun insertarEventos() {
+        controladorEvento.insertarEvento("David", 23.3, "Malaga", "España", 0,  "2020-03-21" , R.drawable.home, 1)
+        controladorEvento.insertarEvento("Fael", 23.3, "Malaga", "España", 20, "2020-03-21" , R.drawable.home, 1)
+        controladorEvento.insertarEvento("Carlos", 23.3, "Malaga", "España", 10,  "2020-03-21" , R.drawable.home, 1)
     }
 }
